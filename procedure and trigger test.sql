3bb2660f100856f1ae0f883615e79479552f7e85
Dmitriy Korotkov <nupellot@yahoo.com>
2022-04-28 21:56:53 +0300

    Причесал код

--- procedure and trigger test.sql
+++ procedure and trigger test.sql
@@ -1,51 +1,49 @@
-use rk6_schema;
+USE rk6_schema;
+SET SQL_SAFE_UPDATES = 0;  # Разрешаем использовать UPDATE без WHERE
 
 # Разработать процедуру, которая будет принимать на вход дату. В процедуре реализовать вычисление и занесение в БД стоимости всех заказов, составленных в этот день.
-drop table report;
-create table report(summ int, dat date);
-select * from report;
-drop procedure daily_cost;
-call daily_cost("2022-04-01");
+DROP TABLE report;  # Таблица, которая будет хранить стомости заказов.
+CREATE TABLE report(summ INT, dat DATE);
+SELECT * FROM report;
+drop PROCEDURE daily_cost;
+CALL daily_cost("2022-04-01");
+
 delimiter //
-create procedure daily_cost(d date)
-begin
-	declare suma int; /*переменная*/
-    declare flag int; /*тоже переменная*/
+CREATE PROCEDURE daily_cost(d DATE)
+BEGIN
+	DECLARE suma INT;
+    DECLARE flag INT;
     
-    select count(*) /*проверяем, были ли уже записи для этой даты*/
-    from report 
-    where dat = d 
-    into flag; 
+    /*проверяем, были ли уже записи для этой даты*/
+    SELECT COUNT(*)  
+    FROM report 
+    WHERE dat = d 
+    INTO flag; 
     
-    select sum(price) from `order` /*суммируем суммы заказов*/
-    where date_preparation = d  /*сделанных в нужный день*/
-    into suma; /* и заносим переменную*/
+    SELECT SUM(price) FROM `order` /*суммируем суммы заказов*/
+    WHERE DATE_preparation = d  /*сделанных в нужный день*/
+    INTO suma; /* и заносим в переменную*/
     
-    if flag<>0  /*если запись за этот день уже была*/
-    then /*тогда*/
-		update report  
-        set summ = suma /*обновляем отчет*/
-        where dat = d;
-	end if;
-    if flag = 0 /*если записи не было*/
-    then
-		insert into report values (suma, d); /*то создаем ее*/
-	end if;
-end//
+    IF (flag <> 0)  /*если запись за этот день уже была*/
+    THEN /*тогда*/
+		UPDATE report  
+        SET summ = suma /*обновляем отчет*/
+        WHERE dat = d;
+	END IF;
+    IF (flag = 0) /*если записи не было*/
+    THEN
+		INSERT INTO report values (SUMa, d); /*то создаем ее*/
+	END IF;
+END
+//
 
 # Реализовать триггер, который будет срабатывать при добавлении новой строки заказа. В триггере реализовать увеличение общей стоимости соответствующего заказа нарастающим образом.
+DROP TRIGGER trigg;
 delimiter \\ 
-create trigger trigg AFTER insert on order_line  for each row
-begin
-    /*declare earn int;/*переменная для стоимости строки*/
-    /*select now_in_store.price from now_on_store /*выбираем цену лекарства*/
-    /*where now_in_store.id_b = new.id_b into earn; /*которое было добавлено со строкой*/
-    /*update `order`  /*обновляем стоимость заказа*/
-    /*set price = price + earn*new.amount прибавляем стоимость строки к заказу*/
-    /*where id_o=new.id_a; /*в который была внесена строка*/
-    update `order`
-    set price = price + new.price*new.amount
-    where id_o=new.id_a;
-end
+CREATE TRIGGER trigg AFTER INSERT on order_line FOR EACH ROW
+BEGIN
+    UPDATE `order`
+    SET price = price + new.price*new.amount
+    WHERE id_o=new.id_a;
+END
 \\
-drop trigger trigg;
