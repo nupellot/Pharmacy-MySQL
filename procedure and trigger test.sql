b3858e6713ace612507efd0bf10875b9d491f064
Dmitriy Korotkov <nupellot@yahoo.com>
2022-05-13 00:16:30 +0300

    Добавлены комментарии

--- procedure and trigger test.sql
+++ procedure and trigger test.sql
@@ -3,13 +3,13 @@ SET SQL_SAFE_UPDATES = 0;  # Разрешаем использовать UPDATE 
 
 # Разработать процедуру, которая будет принимать на вход дату. В процедуре реализовать вычисление и занесение в БД стоимости всех заказов, составленных в этот день.
 DROP TABLE report;  # Таблица, которая будет хранить стомости заказов.
-CREATE TABLE report(summ INT, dat DATE);
+CREATE TABLE report(summ INT, dat DATE);  # Создаем таблицу для хранения стоимостей.
 SELECT * FROM report;
-drop PROCEDURE daily_cost;
-CALL daily_cost("2022-04-01");
+drop PROCEDURE daily_cost;  # Удалить процедуру, если она уже существует.
+CALL daily_cost("2022-03-01");  # Вызов процедуры.
 
 delimiter //
-CREATE PROCEDURE daily_cost(d DATE)
+CREATE PROCEDURE daily_cost(d DATE)  # Создание процедуры.
 BEGIN
 	DECLARE suma INT;
     DECLARE flag INT;
@@ -19,6 +19,9 @@ BEGIN
     FROM report 
     WHERE dat = d 
     INTO flag; 
+    # flag хранит в себе количество уже существующих в таблице отчёта строк с такой датой.
+    # Если это 0, то нужно создать такую запись.
+    # Если это 1, то она уже существует и её нужно просто обновить.
     
     SELECT SUM(price) FROM `order` /*суммируем суммы заказов*/
     WHERE DATE_preparation = d  /*сделанных в нужный день*/
@@ -39,10 +42,10 @@ END
 
 # Реализовать триггер, который будет срабатывать при добавлении новой строки заказа. В триггере реализовать увеличение общей стоимости соответствующего заказа нарастающим образом.
 //
-DROP TRIGGER trigg;
+DROP TRIGGER trigg;  # Удаление триггера.
 //
 delimiter \\ 
-CREATE TRIGGER trigg AFTER INSERT on order_line FOR EACH ROW
+CREATE TRIGGER trigg AFTER INSERT on order_line FOR EACH ROW  # Создание триггера.
 BEGIN
     UPDATE `order`
     SET price = price + new.price*new.amount
